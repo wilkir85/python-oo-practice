69e63f7b33ff54cad174458ff3364f4327cfce4d
wilkir <wilkir85@gmail.com>
2024-02-20 17:07:36 -0500

    commit

---
+++ serial.py
@@ -0,0 +1,40 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start=0):
+        """Start generator"""
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Show representation"""
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+    
+    def generate(self):
+        """Generate next serial number"""
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        """reset to original number"""
+        self.next = self.start
+
+
